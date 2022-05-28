2045d0ba49717b2b3a70b87fdbc2290c36e4510f
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2022-05-29 01:27:19 +0400

    changed functions a bit, finished getting all the data from website

--- scraper-scripts/imperio.py
+++ scraper-scripts/imperio.py
@@ -1,43 +1,52 @@
 from bs4 import BeautifulSoup as bs
-import re
 from typing import List
 import utils
 import os
 from dotenv import load_dotenv
 
 
-def get_names(html_data: str, 
-                html_element: str, 
-                html_element_attrs: str,
-                pattern: str) -> List[str] | bool:
+def get_data(html_data: str) -> List[str]:
     soup = bs(html_data, "html.parser")
-    names = soup.find_all(name=html_element, attrs=html_element_attrs)
-    sushi_set_names_raw = re.findall(pattern, str(names))
+    elements = soup.find_all(name='div', attrs='page-list-ext-item')
+    phone_number = soup.find(id="phone").a.string
 
-    sushi_set_names_clean = []
-    for name in sushi_set_names_raw:
-        sushi_set_names_clean.append(name[1:len(name) - 3])
-    
-    if len(sushi_set_names_clean) == len(names):
-        return sushi_set_names_clean
-    else:
-        return False
+    sushi_set_data = []
+
+    for element in elements:
+        data = {}
+        try:
+            data.update({"title": element.h3.a.get('title')})
+            weight_and_price = element.find_all(name='b')
+            data.update({"weight": f"{weight_and_price[0].string} гр"})
+            data.update({"price": f"{weight_and_price[1].string} руб"})
+            data.update({"img": element.img.get('src')})
+            data.update({"link": element.a.get('href')})
+            data.update({"phone_number": phone_number})
+            sushi_set_data.append(data)
+        except:
+            pass
+    return sushi_set_data
+
 
 load_dotenv()
 
 URL = os.getenv('URL_IMPERIO')
 FILE_NAME = os.getenv('FILE_NAME_IMPERIO')
-html_data = ""
+html_data_new = ""
+html_data_old = ""
 
+html_data_new = utils.get_html_page(URL, FILE_NAME)
 
-if os.path.exists(FILE_NAME + ".html"):
-    print("from file")
-    with open(FILE_NAME + ".html", "r") as file:
-        html_data = file.read()
+with open(FILE_NAME + ".html", "r") as file:
+    html_data_old = file.read()
+
+sushi_imperio_new_data = get_data(html_data_new)
+sushi_imperio_old_data = get_data(html_data_old)
+
+if sushi_imperio_new_data == sushi_imperio_old_data:
+    print("Up to date")
 else:
-    print("from website")
-    html_data = utils.get_html_page(URL, FILE_NAME)
-
-sushi_set_names = get_names(html_data, 'h3', 'page-list-ext-title', '>[^<][А-я\s"1-9#№A-z]*<\/a?')
-print(sushi_set_names)
-print(len(sushi_set_names))
+    with open(FILE_NAME + ".html", "w") as file:
+        file.write(html_data_new)
+    print(sushi_imperio_new_data)
+    # send data to api
